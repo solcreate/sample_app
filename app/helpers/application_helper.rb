e52fa26b6cc5cfeee168892843902a5bda3971bb
kawahara <solcreate@gmail.com>
2014-01-13 17:36:13 +0700

    contacts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    return base_title if page_title.blank?
+    return "#{base_title} | #{page_title}"
+  end
 end
