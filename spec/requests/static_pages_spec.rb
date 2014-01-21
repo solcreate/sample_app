696bd2b0f0dc8da5445ba4d6e2d37ce54618a304
kawahara <solcreate@gmail.com>
2014-01-22 01:48:58 +0700

    hoge

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,18 +4,26 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe 'Home page' do
     before { visit root_path }
-    it { should have_content('Sample App')}
-    it { should have_title(full_title(''))}
-    it { should_not have_title('| Home')}
+
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+
+    it_should_behave_like 'all static pages'
+    it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
     before { visit help_path }
-
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+    it_should_behave_like 'all static pages'
   end
 
   describe "About page" do
@@ -32,4 +40,18 @@ describe "Static pages" do
     it { should have_title(full_title('Contact')) }
   end
 
+  #it "should have the right links on the layout" do
+  #  visit root_path
+  #  click_link "About"
+  #  page.should have_selector 'title', text: full_title('About Us')
+  #  click_link "Help"
+  #  page.should # fill in
+  #  click_link "Contact"
+  #  page.should # fill in
+  #  click_link "Home"
+  #  click_link "Sign up now!"
+  #  page.should # fill in
+  #  click_link "sample app"
+  #  page.should # fill in
+  #end
 end
